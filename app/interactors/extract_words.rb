f7404132da9a0b81db89e5fbf7fca11388464f17
Ramilia Nigmatullina <RamiliaNigmatullina@gmail.com>
2023-06-27 21:41:48 +0200

    use regexp

--- app/interactors/extract_words.rb
+++ app/interactors/extract_words.rb
@@ -1,6 +1,8 @@
 class ExtractWords
   include Interactor
 
+  EXTRACT_WORDS_REGEXP = /^(.+?\.\s)?(.+?)\s(-|—|—).*$/.freeze
+
   delegate :file_text, to: :context
 
   def call
@@ -12,12 +14,16 @@ class ExtractWords
   def words
     [].tap do |words|
       split_text.each do |line|
-        words << line.split(" —").first.split(". ").last.gsub(" ", "-")
+        words << extracted_word(line)
       end
     end
   end
 
   def split_text
     file_text.split("\n")
+  end
+
+  def extracted_word(line)
+    line.gsub(EXTRACT_WORDS_REGEXP, '\2')
   end
 end
