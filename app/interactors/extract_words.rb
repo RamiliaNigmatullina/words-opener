5b14e4cbdbc5b6fd6431eec9f58373ac9dba8169
RamiliaNigmatullina <ramilianigmatullina@gmail.com>
2024-04-02 18:28:12 +0200

    remove code related to reading from file

--- app/interactors/extract_words.rb
+++ app/interactors/extract_words.rb
@@ -3,8 +3,6 @@ class ExtractWords
 
   EXTRACT_WORDS_REGEXP = /^(.+?\.\s)?(.+?)\s(-|—|—).*$/
 
-  delegate :file_text, to: :context
-
   def call
     context.words = words.compact
   end
@@ -12,19 +10,9 @@ class ExtractWords
   private
 
   def words
-    [].tap do |words|
-      split_text.each do |line|
-        words << extracted_word(line)
-      end
+    context.user_input.each_with_object([]) do |line, words|
+      words << extracted_word(line)
     end
-  end
-
-  def split_text
-    user_input_provided? ? context.user_input : file_text.split("\n")
-  end
-
-  def user_input_provided?
-    context.user_input.map { |s| s.gsub("\n", "") }.any?(&:present?)
   end
 
   def extracted_word(line)
